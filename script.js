a019edca9a8b935402f81cdc6902fa9af350d828
DigvijayRana25 <drana6167@gmail.com>
2026-03-18 17:40:18 +0530

    added site map and database

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
+document.getElementById("contactForm").addEventListener("submit", function(e){
+
+e.preventDefault();
+
+const formData = {
+name: this.name.value,
+phone: this.phone.value,
+email: this.email.value,
+requirement: this.requirement.value,
+property_type: this.property_type.value,
+location: this.location.value,
+budget: this.budget.value,
+details: this.details.value
+};
+
+fetch("https://script.google.com/macros/s/AKfycbwfNGf4-OPShjwC0atFzcSoY37xBmibdiFAAA9K--6pYUkIy9YbJTeLkho2rhDbo4z4/exec", {
+method: "POST",
+body: JSON.stringify(formData)
+})
+.then(res => res.json())
+.then(data => {
+
+alert("Thank you! Your request has been recorded.");
+
+document.getElementById("contactForm").reset();
+
+})
+.catch(err => console.log(err));
+
+});
 // Get elements
 const requirement = document.getElementById("requirement");
 const buyFields = document.getElementById("buyFields");
